0d839275b3bfe951666277e98d38895ab51763e8
Mehdi <mehdi.slimani.elmaddarsi@gmail.com>
2025-02-17 08:39:44 +0000

    Added tests
    - 2D 2 materials diffusing
    - 1D non linear conducitvity

---
+++ tests/1d_nnlinear_conductivity/test_1dnnlinearconduc.py
@@ -0,0 +1,180 @@
+from dolfinx import mesh, fem
+from mhs_fenicsx.problem import Problem, set_same_mesh_interface
+from mpi4py import MPI
+import yaml
+import numpy as np
+from mhs_fenicsx.drivers import StaggeredRRDriver, MonolithicRRDriver
+from mhs_fenicsx.problem.helpers import assert_pointwise_vals, print_vals
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def get_u_exact(params):
+    ul, ur = 2.0, 7.0
+    c2 = ul*(ul+2)
+    c1 = (ur*(ur+2) - ul*(ul+2)) / 2.0
+    return lambda x : -1 + np.sqrt(1 + c2 + c1*x[0])
+
+def get_mesh(L):
+    nelems = 20
+    return mesh.create_interval(comm,
+                                nelems,
+                                [0.0, L])
+
+def ref(params, writepos=True):
+    L = params["L"]
+    domain = get_mesh(L)
+    p = Problem(domain, params, "nnlinearconduc")
+    marker  = lambda x : np.logical_or(np.isclose(x[0],0),
+                                       np.isclose(x[0],L))
+    u_d = fem.Function(p.v,name="exact")
+    u_d.interpolate(get_u_exact(params))
+    bdofs_dir  = fem.locate_dofs_geometrical(p.v,marker)
+    p.dirichlet_bcs = [fem.dirichletbc(u_d, bdofs_dir)]
+
+    # Solve
+    p.set_forms()
+    p.compile_create_forms()
+    p.pre_assemble()
+    p.non_linear_solve()
+    p.post_iterate()
+    if writepos:
+        p.writepos(extra_funcs=[u_d])
+    return p
+
+
+def set_bc(pright,pleft):
+    # Set outside Dirichlet
+    params = pright.input_parameters
+    L = params["L"]
+    pright.add_dirichlet_bc(get_u_exact(params), marker=(lambda x : np.isclose(x[0],L)), reset=True)
+    pleft.add_dirichlet_bc(get_u_exact(params), marker=(lambda x : np.isclose(x[0],0)), reset=True)
+    for p in (pright, pleft):
+        p.dirichlet_bcs[0].g.name = "exact"
+
+def staggered_robin(params, writepos=True):
+    L = params["L"]
+    domain = get_mesh(L)
+    p_left = Problem(domain, params, "nnlinearconduc_Left_stag_robin")
+    p_right = p_left.copy(name="nnlinearconduc_Right_stag_robin")
+    # Activation
+    active_els = dict()
+    active_els[p_left] = fem.locate_dofs_geometrical(p_left.dg0, lambda x : x[0] <= L/2.0 )
+    active_els[p_right] = fem.locate_dofs_geometrical(p_right.dg0, lambda x : x[0] >= L/2.0 )
+    for p in [p_left,p_right]:
+        p.set_activation(active_els[p])
+    set_same_mesh_interface(p_left, p_right)
+    driver = StaggeredRRDriver(p_right, p_left, max_staggered_iters=20,
+                               initial_relaxation_factors=[1.0, 1.0])
+
+    driver.pre_loop(set_bc=set_bc)
+    for _ in range(driver.max_staggered_iters):
+        driver.pre_iterate()
+        driver.iterate()
+        driver.post_iterate(verbose=True)
+        if writepos:
+            driver.writepos()
+        if driver.convergence_crit < driver.convergence_threshold:
+            break
+    driver.post_loop()
+    return p_left, p_right, driver.iter
+
+def monolithic_robin(params, writepos=True):
+    L = params["L"]
+    domain = get_mesh(L)
+    p_left = Problem(domain, params, "nnlinearconduc_Left_mono_robin")
+    p_right = p_left.copy(name="nnlinearconduc_Right_mono_robin")
+    # Activation
+    active_els = dict()
+    active_els[p_left] = fem.locate_dofs_geometrical(p_left.dg0, lambda x : x[0] <= L/2.0 )
+    active_els[p_right] = fem.locate_dofs_geometrical(p_right.dg0, lambda x : x[0] >= L/2.0 )
+    for p in [p_left,p_right]:
+        p.set_activation(active_els[p])
+    set_same_mesh_interface(p_left, p_right)
+    set_bc(p_right, p_left)
+    driver = MonolithicRRDriver(p_left, p_right, quadrature_degree=2)
+    for p in [p_left, p_right]:
+        p.set_forms()
+        p.compile_create_forms()
+        p.pre_assemble()
+    driver.non_linear_solve()
+    driver.post_iterate()
+    for p in [p_left, p_right]:
+        p.post_iterate()
+        if writepos:
+            p.writepos(extra_funcs=[p.dirichlet_bcs[0].g])
+    return p_left, p_right
+
+def test_1dnnlinearconduc_ref():
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    p = ref(params=params, writepos=False)
+    points = np.array([
+        [+0.25, 0.0, 0.0],
+        [+1.00, 0.0, 0.0],
+        [+1.50, 0.0, 0.0],
+        ])
+    vals = np.array([
+        2.9805992,
+        5.041523,
+        6.0887234])
+    assert_pointwise_vals(p,points,vals)
+
+def test_1dnnlinearconduc_stag_robin():
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    pl, pr, num_its = staggered_robin(params=params, writepos=False)
+    assert(num_its == 19)
+    points = {
+            pl : np.array([
+                [+0.25, 0.0, 0.0],
+                [+1.00, 0.0, 0.0],
+                ]),
+            pr : np.array([
+                [+1.00, 0.0, 0.0],
+                [+1.50, 0.0, 0.0],
+                ]),
+            }
+    vals = {
+            pl : np.array([
+                2.9733266,
+                5.0222116,]),
+            pr : np.array([
+                5.0174559,
+                6.0784806,]),
+            }
+    for p in [pl, pr]:
+        assert_pointwise_vals(p, points[p], vals[p])
+
+
+def test_1dnnlinearconduc_mono_robin():
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    pl, pr = monolithic_robin(params=params, writepos=False)
+    points = {
+            pl : np.array([
+                [+0.25, 0.0, 0.0],
+                [+1.00, 0.0, 0.0],
+                ]),
+            pr : np.array([
+                [+1.00, 0.0, 0.0],
+                [+1.50, 0.0, 0.0],
+                ]),
+            }
+    vals = {
+            pl : np.array([
+                2.9733277,
+                5.0222146,]),
+            pr : np.array([
+                5.0174515,
+                6.0784788,]),
+            }
+    for p in [pl, pr]:
+        assert_pointwise_vals(p, points[p], vals[p])
+
+if __name__=="__main__":
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    ref(params)
+    staggered_robin(params)
+    monolithic_robin(params)
--- tests/2mats_diffusing_square/test_2mats_diffusing_square.py
+++ tests/2mats_diffusing_square/test_2mats_diffusing_square.py
@@ -4,19 +4,20 @@ from mpi4py import MPI
 import yaml
 import numpy as np
 from mhs_fenicsx.drivers import StaggeredRRDriver, MonolithicRRDriver
+from mhs_fenicsx.problem.helpers import assert_pointwise_vals, print_vals
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-u_hot = 7.0
-u_cold = 2.0
-kl = 1.0
-kr = 10.0
-c1 = (u_hot - u_cold) / (0.5 + (kl / kr)*0.5)
-c2 = kl / kr * c1
-d2 = u_cold + 0.5 * (c1 - c2)
 
 def u_exact(x):
+    u_hot = 7.0
+    u_cold = 2.0
+    kl = 1.0
+    kr = 10.0
+    c1 = (u_hot - u_cold) / (0.5 + (kl / kr)*0.5)
+    c2 = kl / kr * c1
+    d2 = u_cold + 0.5 * (c1 - c2)
     left = np.array(x[0] <= 0.5, dtype=np.float64)*(u_cold + c1 * x[0])
     right = np.array(x[0] > 0.5, dtype=np.float64)*(d2 + c2*x[0])
     return (left + right)
@@ -27,7 +28,7 @@ def get_mesh():
                                    nelems_side, nelems_side,
                                    )
 
-def ref(params):
+def ref(params, writepos=True):
     domain = get_mesh()
     p = Problem(domain, params, "2mat_difussing")
     marker  = lambda x : np.logical_or(np.isclose(x[0],0.0),
@@ -57,7 +58,7 @@ def set_bc(pright,pleft):
     for p in (pright, pleft):
         p.dirichlet_bcs[0].g.name = "exact"
 
-def staggered_robin(params):
+def staggered_robin(params, writepos=True):
     domain = get_mesh()
     p_left = Problem(domain, params, "2mat_diffusion_Left_stag_robin")
     right_els = fem.locate_dofs_geometrical(p_left.dg0, lambda x : x[0] >= 0.5 )
@@ -83,9 +84,9 @@ def staggered_robin(params):
         if driver.convergence_crit < driver.convergence_threshold:
             break
     driver.post_loop()
-    return p_left, p_right
+    return p_left, p_right, driver.iter
 
-def monolithic_robin(params):
+def monolithic_robin(params, writepos=True):
     domain = get_mesh()
     p_left = Problem(domain, params, "2mat_diffusion_Left_mono_robin")
     right_els = fem.locate_dofs_geometrical(p_left.dg0, lambda x : x[0] >= 0.5 )
@@ -111,9 +112,76 @@ def monolithic_robin(params):
         p.writepos(extra_funcs=[p.dirichlet_bcs[0].g])
     return p_left, p_right
 
-if __name__=="__main__":
-    with open("input.yaml", 'r') as f:
+def test_2mats_diffusing_square_ref():
+    with open("test_input.yaml", 'r') as f:
         params = yaml.safe_load(f)
-    #ref(params)
+    p = ref(params=params, writepos=False)
+    points = np.array([
+        [0.25, 0.25, 0.0],
+        [0.50, 0.50, 0.0],
+        [0.75, 0.75, 0.0],
+        ])
+    vals = np.array([
+        4.2727273,
+        6.5454545,
+        6.7727273])
+    assert_pointwise_vals(p,points,vals)
+
+def test_2mats_diffusing_square_stag_robin():
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    pl, pr, num_its = staggered_robin(params=params, writepos=False)
+    assert(num_its == 12)
+    points = {
+            pl : np.array([
+                [0.25, 0.25, 0.0],
+                [0.50, 0.50, 0.0],
+                ]),
+            pr : np.array([
+                [0.25, 0.25, 0.0],
+                [0.75, 0.75, 0.0],
+                ]),
+            }
+    vals = {
+            pl : np.array([
+                4.2727266,
+                6.5454531,]),
+            pr : np.array([
+                6.5454548,
+                6.7727274,]),
+            }
+    for p in [pl, pr]:
+        assert_pointwise_vals(p, points[p], vals[p])
+
+
+def test_2mats_diffusing_square_mono_robin():
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    pl, pr = monolithic_robin(params=params, writepos=False)
+    points = {
+            pl : np.array([
+                [0.25, 0.25, 0.0],
+                [0.50, 0.50, 0.0],
+                ]),
+            pr : np.array([
+                [0.25, 0.25, 0.0],
+                [0.75, 0.75, 0.0],
+                ]),
+            }
+    vals = {
+            pl : np.array([
+                4.2727273,
+                6.5454545,]),
+            pr : np.array([
+                6.5454545,
+                6.7727273,]),
+            }
+    for p in [pl, pr]:
+        assert_pointwise_vals(p, points[p], vals[p])
+
+if __name__=="__main__":
+    with open("test_input.yaml", 'r') as f:
+        params = yaml.safe_load(f)
+    ref(params)
     staggered_robin(params)
-    #monolithic_robin(params)
+    monolithic_robin(params)

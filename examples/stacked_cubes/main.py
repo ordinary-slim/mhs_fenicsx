561b2d450a7e320bb74c545dc58ca521943307c2
Mehdi Slimani <mehdi.s0kt@gmail.com>
2025-07-07 17:27:53 +0000

    building lpbf perf case

---
+++ examples/stacked_cubes/main.py
@@ -0,0 +1,183 @@
+import numpy as np
+import yaml
+from dolfinx import mesh
+from mpi4py import MPI
+from mesh import create_stacked_cubes_mesh
+from mhs_fenicsx.problem import Problem
+from mhs_fenicsx.gcode import TrackType
+from mhs_fenicsx.chimera import build_moving_problem
+from mhs_fenicsx.drivers.substeppers import MHSSubstepper, MHSStaggeredSubstepper, MHSSemiMonolithicSubstepper, ChimeraSubstepper, MHSStaggeredChimeraSubstepper, MHSSemiMonolithicChimeraSubstepper
+from mhs_fenicsx.drivers import MonolithicRRDriver, MonolithicDomainDecompositionDriver, StaggeredRRDriver
+import argparse
+from line_profiler import LineProfiler
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def write_gcode(params):
+    num_layers = params["num_layers"]
+    layer_thickness = params["layer_thickness"]
+    hatch_spacing = params["hatch_spacing"]
+    width = params["width"]
+    half_len = width / 2.0
+    num_hatches = np.rint(width / hatch_spacing).astype(int)
+    speed = np.linalg.norm(np.array(params["source_terms"][0]["initial_speed"]))
+    gcode_lines = []
+    gcode_lines.append(f"G0 F{speed:g}")
+    p0, p1 = np.zeros(3), np.zeros(3)
+    E = 0.0
+    for ilayer in range(num_layers):
+        if ilayer % 2 == 0:
+            const_idx = 0
+            mov_idx = 1
+        else:
+            const_idx = 1
+            mov_idx = 0
+        z = layer_thickness * (ilayer + 1)
+        p0[2], p1[2] = z, z
+        for ihatch in range(num_hatches):
+            E += 0.1
+            fixed_coord = -half_len + (ihatch + 0.5) * layer_thickness
+            sign = (ihatch + 1) % 2
+            mov_coord0 = (-1)**sign * half_len
+            mov_coord1 = -mov_coord0
+            p0[const_idx], p1[const_idx] = fixed_coord, fixed_coord
+            p0[mov_idx], p1[mov_idx] = mov_coord0, mov_coord1
+            if ihatch==0:
+                gcode_lines.append(f"G4 X{p0[0]:g} Y{p0[1]:g} Z{z} P0.5")
+                gcode_lines.append(f"G4 P0.5 R1")
+            else:
+                positionning_line = f"G0 X{p0[0]:g} Y{p0[1]:g}"
+                gcode_lines.append(positionning_line)
+            printing_line = f"G1 X{p1[0]:g} Y{p1[1]:g} E{E:g}"
+            gcode_lines.append(printing_line)
+
+    gcode_file = params["source_terms"][0]["path"]
+    with open(gcode_file,'w') as f:
+        f.writelines("\n".join(gcode_lines))
+
+def get_k(p):
+    return p.materials[0].k.Ys[:-1].mean()
+
+def run_reference(params, descriptor=""):
+    writepos = params.get("writepos", True)
+    domain = create_stacked_cubes_mesh(params)
+    params["petsc_opts"] = params["petsc_opts_macro"]
+    ps = Problem(domain, params, name="ref" + descriptor)
+
+    ps.set_initial_condition(  params["environment_temperature"] )
+
+    # Deactivate below surface
+    midpoints_cells = mesh.compute_midpoints(ps.domain, ps.dim, np.arange(ps.num_cells))
+    substrate_els = (midpoints_cells[:, 2] <= 0.0).nonzero()[0]
+    ps.set_activation(substrate_els, finalize=True)
+    ps.update_material_at_cells(substrate_els, ps.materials[1])
+
+    ps.set_forms()
+    ps.compile_forms()
+    adim_dt_print = params["substepping_parameters"]["micro_adim_dt"]
+    adim_dt_cooling = params["substepping_parameters"]["cooling_adim_dt"]
+    itime_step = 0
+    max_timesteps = params.get("max_timesteps", 1e9)
+    while (not(ps.is_path_over()) and itime_step < max_timesteps):
+        itime_step += 1
+        track = ps.source.path.get_track(ps.time)
+        if ps.source.path.get_track(ps.time).type in [TrackType.RECOATING,
+                                                      TrackType.DWELLING]:
+            ps.set_dt(ps.dimensionalize_waiting_timestep(track, adim_dt_cooling))
+        else:
+            ps.set_dt(ps.dimensionalize_mhs_timestep(track, adim_dt_print))
+        ps.pre_iterate()
+        ps.instantiate_forms()
+        ps.pre_assemble()
+        ps.non_linear_solve()
+        ps.post_iterate()
+        if writepos:
+            ps.writepos(extension="vtx", extra_funcs=[ps.u_av])
+    return ps
+
+def run_staggered(params, descriptor=""):
+    writepos = params.get("writepos", True)
+    domain = create_stacked_cubes_mesh(params)
+
+    macro_params = params.copy()
+    macro_params["petsc_opts"] = macro_params["petsc_opts_macro"]
+    ps = Problem(domain, macro_params, finalize_activation=False, name="staggered_rr" + descriptor)
+    ps.set_initial_condition(  params["environment_temperature"] )
+
+
+    substeppin_driver = MHSStaggeredSubstepper(StaggeredRRDriver,
+                                               [1.0, 1.0],
+                                               ps,)
+    ps = substeppin_driver.ps
+    staggered_driver = substeppin_driver.staggered_driver
+    staggered_driver.set_dirichlet_coefficients(
+            params["fine_el_size"], get_k(ps))
+
+    max_timesteps = params.get("max_timesteps", 1e9)
+    itime_step = 0
+    while ((itime_step < max_timesteps) and not(ps.is_path_over())):
+        itime_step += 1
+        substeppin_driver.do_timestep()
+        if writepos:
+            ps.writepos(extension="vtx", extra_funcs=[ps.u_prev])
+    return ps
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--run-ref', action='store_true')
+    parser.add_argument('-ss','--run-stagg-sub', action='store_true')
+    parser.add_argument('-sms','--run-hodge', action='store_true')
+    parser.add_argument('-css','--run-chimera-stagg', action='store_true')
+    parser.add_argument('-csms','--run-chimera-hodge', action='store_true')
+    parser.add_argument('-d','--descriptor', default="")
+    lp = LineProfiler()
+    lp.add_module(Problem)
+    args = parser.parse_args()
+    if args.run_chimera_stagg or args.run_chimera_hodge:
+        params_file = "chimera_input.yaml"
+    else:
+        params_file = "input.yaml"
+    with open(params_file, 'r') as f:
+        params = yaml.safe_load(f)
+    write_gcode(params)
+    profiling_file = ""
+    if args.run_ref:
+        lp_wrapper = lp(run_reference)
+        lp_wrapper(params, descriptor = args.descriptor)
+        profiling_file = f"profiling_ref_{rank}.txt"
+    if args.run_stagg_sub:
+        lp.add_module(MHSSubstepper)
+        lp.add_module(MHSStaggeredSubstepper)
+        lp.add_module(StaggeredRRDriver)
+        lp_wrapper = lp(run_staggered)
+        lp_wrapper(params, descriptor = args.descriptor)
+        profiling_file = f"profiling_ss_{rank}.txt"
+    if args.run_hodge:
+        lp.add_module(MHSSubstepper)
+        lp.add_module(MHSSemiMonolithicSubstepper)
+        lp_wrapper = lp(run_hodge)
+        lp_wrapper(params, descriptor = args.descriptor)
+        profiling_file = f"profiling_sms_{rank}.txt"
+    if args.run_chimera_stagg:
+        lp.add_module(MHSStaggeredChimeraSubstepper)
+        lp.add_module(MHSStaggeredSubstepper)
+        lp.add_module(MHSSubstepper)
+        lp.add_module(MonolithicDomainDecompositionDriver)
+        lp.add_module(MonolithicRRDriver)
+        lp.add_module(ChimeraSubstepper)
+        lp_wrapper = lp(run_staggered_chimera_rr)
+        lp_wrapper(params, descriptor = args.descriptor)
+        profiling_file = f"profiling_chimera_rss_{rank}.txt"
+    if args.run_chimera_hodge:
+        lp.add_module(MHSSubstepper)
+        lp.add_module(MHSSemiMonolithicChimeraSubstepper)
+        lp.add_module(MonolithicDomainDecompositionDriver)
+        lp.add_module(MonolithicRRDriver)
+        lp.add_module(ChimeraSubstepper)
+        lp_wrapper = lp(run_chimera_hodge)
+        lp_wrapper(params, descriptor = args.descriptor)
+        profiling_file = f"profiling_chimera_hodge_{rank}.txt"
+    if profiling_file:
+        with open(profiling_file, 'w') as pf:
+            lp.print_stats(stream=pf)
---
+++ examples/stacked_cubes/mesh.py
@@ -0,0 +1,133 @@
+import gmsh
+import yaml
+running_w_dolfinx = True
+try:
+    from dolfinx.io.gmshio import model_to_mesh
+    from dolfinx.mesh import GhostMode, create_cell_partitioner
+except ImportError:
+    running_w_dolfinx = False
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+def create_stacked_cubes_mesh(params):
+    gmsh.initialize()
+    if rank == 0:
+        gmsh.model.add("stacked_cubes")
+
+        fine_el_size = params["fine_el_size"]
+        coarse_el_size = params["coarse_el_size"]
+        layer_thickness = params["layer_thickness"]
+        num_layers = params["num_layers"]
+        num_buffer_layers = params["num_buffer_layers"]
+        substrate_depth = params["substrate_depth"]
+        L = params["width"]
+        part_height = layer_thickness * num_layers
+        buffer_depth = layer_thickness * num_buffer_layers
+
+        # ---------------- Points ----------------
+        coords = {
+            1: (-L/2, -L/2, -buffer_depth, fine_el_size),
+            2: (+L/2, -L/2, -buffer_depth, fine_el_size),
+            3: (+L/2, +L/2, -buffer_depth, fine_el_size),
+            4: (-L/2, +L/2, -buffer_depth, fine_el_size),
+            5: (-L/2, -L/2, part_height, fine_el_size),
+            6: (+L/2, -L/2, part_height, fine_el_size),
+            7: (+L/2, +L/2, part_height, fine_el_size),
+            8: (-L/2, +L/2, part_height, fine_el_size),
+             9: (-L/2, -L/2, -substrate_depth, coarse_el_size),
+            10: (+L/2, -L/2, -substrate_depth, coarse_el_size),
+            11: (+L/2, +L/2, -substrate_depth, coarse_el_size),
+            12: (-L/2, +L/2, -substrate_depth, coarse_el_size),
+        }
+
+        for tag, (x, y, z, meshSize) in coords.items():
+            gmsh.model.geo.addPoint(x, y, z, tag=tag, meshSize=meshSize)
+
+        # ---------------- Lines ----------------
+        lines = [
+            (1,1,2), (2,2,3), (3,3,4), (4,4,1),
+            (5,5,6), (6,6,7), (7,7,8), (8,8,5),
+            (9,1,5), (10,2,6), (11,3,7), (12,4,8),
+            (13,9,10), (14,10,11), (15,11,12), (16,12,9),
+            (17,9,1), (18,10,2), (19,11,3), (20,12,4),
+        ]
+        for tag, p1, p2 in lines:
+            gmsh.model.geo.addLine(p1, p2, tag)
+
+        # ---------------- Surfaces for upper cube ----------------
+        gmsh.model.geo.addCurveLoop([1,2,3,4], 21)
+        gmsh.model.geo.addPlaneSurface([21], 21)
+
+        gmsh.model.geo.addCurveLoop([5,6,7,8], 22)
+        gmsh.model.geo.addPlaneSurface([22], 22)
+
+        gmsh.model.geo.addCurveLoop([1,10,-5,-9],23)
+        gmsh.model.geo.addPlaneSurface([23],23)
+
+        gmsh.model.geo.addCurveLoop([2,11,-6,-10],24)
+        gmsh.model.geo.addPlaneSurface([24],24)
+
+        gmsh.model.geo.addCurveLoop([3,12,-7,-11],25)
+        gmsh.model.geo.addPlaneSurface([25],25)
+
+        gmsh.model.geo.addCurveLoop([4,9,-8,-12],26)
+        gmsh.model.geo.addPlaneSurface([26],26)
+
+        gmsh.model.geo.addSurfaceLoop([21,22,23,24,25,26],30)
+        gmsh.model.geo.addVolume([30],1)
+
+        # ---------------- Surfaces for lower cube ----------------
+        gmsh.model.geo.addCurveLoop([13,14,15,16],27)
+        gmsh.model.geo.addPlaneSurface([27],27)
+
+        gmsh.model.geo.addCurveLoop([1,-18,-13,+17],29)
+        gmsh.model.geo.addPlaneSurface([29],29)
+
+        gmsh.model.geo.addCurveLoop([2,-19,-14,+18],30)
+        gmsh.model.geo.addPlaneSurface([30],30)
+
+        gmsh.model.geo.addCurveLoop([3,-20,-15,+19],31)
+        gmsh.model.geo.addPlaneSurface([31],31)
+
+        gmsh.model.geo.addCurveLoop([4,-17,-16,+20],32)
+        gmsh.model.geo.addPlaneSurface([32],32)
+
+        gmsh.model.geo.addSurfaceLoop([27,21,29,30,31,32],40)
+        gmsh.model.geo.addVolume([40],2)
+
+        # ---------------- Mesh settings ----------------
+        fine_el_size = params["fine_el_size"]
+        n_xy = int(L / fine_el_size) + 1
+        n_z = int((part_height + buffer_depth) / fine_el_size) + 1
+        for curve in [1,2,3,4,5,6,7,8]:
+            gmsh.model.geo.mesh.setTransfiniteCurve(curve, n_xy)
+        for curve in [9,10,11,12]:
+            gmsh.model.geo.mesh.setTransfiniteCurve(curve, n_z)
+        for surface in [21,22,23,24,25,26]:
+            gmsh.model.geo.mesh.setTransfiniteSurface(surface)
+
+        # ---------------- Mesh generation ----------------
+        gmsh.model.geo.synchronize()
+        gmsh.model.addPhysicalGroup(3, [1, 2], name="Domain")
+        gmsh.model.mesh.generate(3)
+
+    if __name__ == "__main__":
+        gmsh.fltk.run()
+        gmsh.finalize()
+        exit()
+    elif running_w_dolfinx:
+        model = MPI.COMM_WORLD.bcast(gmsh.model, root = 0)
+        partitioner = create_cell_partitioner(GhostMode.shared_facet)
+        msh_data = model_to_mesh(model, MPI.COMM_WORLD, 0,
+                                 gdim=3, partitioner=partitioner)
+        msh = msh_data[0]
+        gmsh.finalize()
+        MPI.COMM_WORLD.barrier()
+        return msh
+
+if __name__ == "__main__":
+    with open("input.yaml", "r") as f:
+        params = yaml.safe_load(f)
+    create_stacked_cubes_mesh(params)
